a4a25cdb1d2c4dfdd4dadeb901d9903688a301a3
Nipun Avishka De Zoysa <nipunavishka123@gmail.com>
2024-06-17 01:34:00 +0530

    Update index.tsx

--- src/react-melon/index.tsx
+++ src/react-melon/index.tsx
@@ -2,24 +2,25 @@ import React from "react";
 import ConfirmBox from "./ConfirmBox";
 import ReactDOM from "react-dom";
 
-export function showConfirm(){
-    return new Promise((resolve, reject) => {
-        const confirmBox = <ConfirmBox resolve={resolve} />;
-        const root = document.getElementById("root");
-        if (root) {
-            const div = document.createElement("div");
-            div.style.position = "fixed";
-            div.style.top = "0";
-            div.style.left = "0";
-            div.style.width = "100%";
-            div.style.height = "100%";
-            div.style.backgroundColor = "rgba(0,0,0,0.5)";
-            div.style.display = "flex";
-            div.style.justifyContent = "center";
-            div.style.alignItems = "center";
-            div.style.zIndex = "1000";
-            root.appendChild(div);
-            ReactDOM.render(confirmBox, div);
-        }
-    });
+export function showConfirm() {
+  const root = document.getElementById("root");
+  const div = document.createElement("div");
+  if (root) {
+    div.style.position = "fixed";
+    div.style.top = "0";
+    div.style.left = "0";
+    div.style.width = "100%";
+    div.style.height = "100%";
+    div.style.zIndex = "1000";
+    root.appendChild(div);
+  }
+
+  return new Promise((resolve, reject) => {
+    if (root) {
+      const confirmBox = <ConfirmBox resolve={resolve} />;
+      ReactDOM.render(confirmBox, div);
+    } else {
+      reject("root not found");
+    }
+  });
 }
